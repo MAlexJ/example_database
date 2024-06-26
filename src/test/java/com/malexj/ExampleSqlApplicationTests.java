f19dfb2821a3a782a2ee4fe93fc78ca7e7624627
malexj <alexmaximovj@gmail.com>
2024-06-26 18:15:57 +0300

    Add modules

--- src/test/java/com/malexj/ExampleSqlApplicationTests.java
+++ src/test/java/com/malexj/ExampleSqlApplicationTests.java
@@ -1,13 +1,8 @@
 package com.malexj;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+// @SpringBootTest
 class ExampleSqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  //	@Test
+  void contextLoads() {}
 }
