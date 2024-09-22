bc93e0ae9e92bb2552979e53dee573781d680faa
malexj <alexmaximovj@gmail.com>
2024-09-22 23:44:56 +0300

    Add tests

--- spring_data_jpa/one_to_one_bidirectional/src/test/java/com/malex/one_to_one_bidirectional/OneToOneBidirectionalApplicationTests.java
+++ spring_data_jpa/one_to_one_bidirectional/src/test/java/com/malex/one_to_one_bidirectional/OneToOneBidirectionalApplicationTests.java
@@ -1,13 +1,14 @@
 package com.malex.one_to_one_bidirectional;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import com.malex.one_to_one_bidirectional.entity.Product;
 import com.malex.one_to_one_bidirectional.entity.Stock;
 import com.malex.one_to_one_bidirectional.entity.StockStatus;
 import com.malex.one_to_one_bidirectional.repository.ProductRepository;
 import com.malex.one_to_one_bidirectional.repository.StockRepository;
+import java.util.List;
 import java.util.logging.Logger;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -51,7 +52,46 @@ class OneToOneBidirectionalApplicationTests {
     Stock persistStock = stockRepository.findById(stockId).orElseThrow();
     assertNotNull(persistStock);
     assertNotNull(persistStock.getProduct());
+  }
 
-    // todo test findALl => N+1
+  @Test
+  void findAllStocks() {
+    Stock appleStock = Stock.builder().stockStatus(StockStatus.IN_STOCK).stockQuantity(3).build();
+    Stock macStock = Stock.builder().stockStatus(StockStatus.IN_STOCK).stockQuantity(3).build();
+    Stock samsungStock = Stock.builder().stockStatus(StockStatus.IN_STOCK).stockQuantity(3).build();
+
+    Product apple =
+        Product.builder().productName("Apple").productPrice(1000).stock(appleStock).build();
+    Product mac = Product.builder().productName("Mac").productPrice(1000).stock(macStock).build();
+    Product samsung =
+        Product.builder().productName("Samsung").productPrice(1000).stock(samsungStock).build();
+
+    /*
+     * SQL: save product with stock
+     * Hibernate: insert into stock (stock_quantity,stock_status,stock_id) values (?,?,default)
+     * Hibernate: insert into product (product_name,product_price,stock_id,product_id) values (?,?,?,default)
+     */
+    productRepository.saveAll(List.of(apple, mac, samsung));
+
+    logger.info("Find stock all");
+    /*
+     * SQL:  findALl => N+1
+     * Hibernate:
+     * select s1_0.stock_id,s1_0.stock_quantity,s1_0.stock_status
+     *    from stock s1_0
+     *
+     * Hibernate:
+     * select p1_0.product_id,p1_0.product_name,p1_0.product_price,
+     *            s1_0.stock_id,s1_0.stock_quantity,s1_0.stock_status
+     *    from product p1_0
+     *    left join stock s1_0 on s1_0.stock_id=p1_0.stock_id
+     *    where p1_0.stock_id=?
+     * Hibernate: select p1_0.product_id,p1_0.product_name,p1_0.product_price,s1_0.stock_id,s1_0.stock_quantity,s1_0.stock_status
+     *    from product p1_0 left join stock s1_0 on s1_0.stock_id=p1_0.stock_id where p1_0.stock_id=?
+     * Hibernate: select p1_0.product_id,p1_0.product_name,p1_0.product_price,s1_0.stock_id,s1_0.stock_quantity,s1_0.stock_status
+     *    from product p1_0 left join stock s1_0 on s1_0.stock_id=p1_0.stock_id where p1_0.stock_id=?
+     */
+    List<Stock> all = stockRepository.findAll();
+    logger.info("Stocks " + all);
   }
 }
