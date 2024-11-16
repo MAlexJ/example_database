7aed0f565b2ac0bbe53b8756bb31d989b469bc1e
malexj <alexmaximovj@gmail.com>
2024-11-16 22:31:41 +0200

    up version

---
+++ spring_data_jpa/many_to_many_bidirectional/src/main/java/com/malex/many_to_many_bidirectional/ManyToManyBidirectionalApplication.java
@@ -0,0 +1,12 @@
+package com.malex.many_to_many_bidirectional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyBidirectionalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ManyToManyBidirectionalApplication.class, args);
+  }
+}
---
+++ spring_data_jpa/many_to_many_bidirectional/src/test/java/com/malex/many_to_many_bidirectional/ManyToManyBidirectionalApplicationTests.java
@@ -0,0 +1,11 @@
+package com.malex.many_to_many_bidirectional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyBidirectionalApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
